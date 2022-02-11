59c0f754292415cb55d8766387d561188b841d87
Estefany Diaz <estefanydiaz75@gmail.com>
2022-02-11 15:25:04 -0500

    pull 3

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-
 export default function Home() {
-    return (
-        <div className="home">
-            <h1>Probando</h1>
-        </div>
-    )
+  return (
+    <div className="home">
+      <h1>Probando 1 2 3 me escuchan?</h1>
+    </div>
+  );
 }

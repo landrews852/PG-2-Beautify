0e49b920708a09b672ca7d5789d15e7d0671c383
AMBohoVal <auribohorquez@gmail.com>
2022-02-11 15:25:31 -0500

    Cambio home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -6,6 +6,8 @@ export default function Home() {
     return (
         <div className="home">
             <h1>Probando</h1>
+            <h2>Mensaje 1</h2>
+            <h3>Heee</h3>
         </div>
     )
 }

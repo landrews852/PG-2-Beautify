ba721fdcdd6f6bd08e6431878fc06c91076633a2
Jair Avila <jair_p1020@hotmail.com>
2022-02-15 18:09:48 -0500

    render Home

--- client/src/components/cardCateServices/index.js
+++ client/src/components/cardCateServices/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 
-export default function CardServices({img,nameservice}) {
+export default function CardCateServices({img,servicio}) {
     return (
         <div className="contenedor">
+
             <div className="cards">               
                 <img src={img} alt="Img not found"/>
-                <h3>{nameservice}</h3>
+                <h3>{servicio}</h3>
             </div>
         </div>
     )
--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -3,8 +3,12 @@ import { Link } from 'react-router-dom';
 import Card from '../card';
 import Slideshow from '../carousel';
 import Navigator from '../navbar';
-import {allProducts} from '../../redux/actions'
+/* import {allProducts} from '../../redux/actions' */
 import Footer from '../footer';
+import CardCateServices from '../cardCateServices';
+let services = require('../../datamock/Services.json')
+let allProducts = require('../../datamock/Products.json')
+
 
 
 function Home() {
@@ -12,24 +16,34 @@ function Home() {
     <div className='home'>
       <Navigator />
       <Slideshow />
-      <Footer />
+      {
+        services && services.slice(0,3).map (e =>(
+          <Link key={e.id_service} to ={'home/service'+ e.id_service}>
+            <CardCateServices
+            img= {e.image}
+            servicio={e.category}
+            />
+          </Link>
+        ))
+      }
 
     {/* COMENTO LO DE ABAJO HASTA QUE SE CREE LA RUTA DE PRODUCTOS ASI NO SE ROMPE LA PAGINA ENTERA */}
 
-    {/*  {allProducts && allProducts.map(c => {
-        return (
-          <Link key={c.id} to={'home/' + c.id}>
-            <Card
-              name={c.name}
-              image={c.image}
-              cost_by_unit={c.cost_by_unit}
-              id={c.id}
-            />
-          </Link>
+     {allProducts && allProducts.map(c => {
+      return (
+        <Link key={c.code} to={'home/' + c.code}>
+        <Card
+        name={c.product_name}
+        image={c.image}
+        cost_by_unit={c.cost_by_unit}
+        id={c.code}
+        />
+        </Link>
         )
       })
-      }
- */}
+    }
+ 
+      <Footer/>
     </div>
   )
 }

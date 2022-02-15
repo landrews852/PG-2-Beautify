4f4910e99b87035986ea508dbb9c18c396b9a561
Estefany Diaz <estefanydiaz75@gmail.com>
2022-02-15 09:52:04 -0500

    relation products-orders

--- api/src/db.js
+++ api/src/db.js
@@ -37,13 +37,16 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Product, Category } = sequelize.models;
+const { Product, Category, Order } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
 
 Product.belongsToMany(Category, { through: "product_category" });
 Category.belongsToMany(Product, { through: "product_category" });
+
+Product.belongsToMany(Order, { through: "order_product" });
+Order.belongsToMany(Product, { through: "order_product" });
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');

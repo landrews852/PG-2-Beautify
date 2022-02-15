4b9d4dc1f867c0ba31e0e45f99bf194219d0cf36
Estefany Diaz <estefanydiaz75@gmail.com>
2022-02-15 09:08:16 -0500

    model/order, change DB name

--- api/src/models/Order.js
+++ api/src/models/Order.js
@@ -1,1 +1,32 @@
-// Esto es order
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "order",
+    {
+      id_order: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      order_date: {
+        type: DataTypes.DATE,
+      },
+      delivery_date: {
+        type: DataTypes.DATE,
+      },
+      total_amount: {
+        type: DataTypes.BOOLEAN,
+      },
+      discount: {
+        type: DataTypes.INTEGER,
+      },
+      status: {
+        type: DataTypes.ENUM("paid", "process", "rejected", "delivered"),
+      },
+    },
+    {
+      timestamps: false,
+    }
+  );
+};

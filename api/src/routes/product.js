3332566797036f48b5374ed5540906a1270d77a3
Estefany Diaz <estefanydiaz75@gmail.com>
2022-02-15 14:34:58 -0500

    route get/product created

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -1,38 +1,79 @@
-const { Router } = require('express');
-const { Product, Category } = require('../db');
+const { Router } = require("express");
+const { Product, Category } = require("../db");
 
 const router = Router();
 
-router.post('/',async (req,res) =>{
+router.get("/", async (req, res) => {
+  try {
+    const allProduct = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ["name_category"],
+          through: {
+            attributes: [],
+          },
+        },
+      ],
+    });
+    res.json(allProduct);
+  } catch (err) {
+    return err;
+  }
+});
 
-    const { product_name, stock, cost_by_unit, description, category, expiration_date, warranty, brand, image, country } = req.body;
+router.post("/", async (req, res) => {
+  const {
+    product_name,
+    stock,
+    cost_by_unit,
+    description,
+    category,
+    expiration_date,
+    warranty,
+    brand,
+    image,
+    country,
+  } = req.body;
 
-    try{
-        if(product_name&&stock&&cost_by_unit&&description&&category&&expiration_date&&warranty&&brand&&image&&country){
-            console.log(req.body)
-            let product = await Product.create({
-                 product_name, 
-                 stock, 
-                 cost_by_unit, 
-                 description,  
-                 expiration_date, 
-                 warranty, 
-                 brand, 
-                 image, 
-                 country 
-            })
+  try {
+    if (
+      product_name &&
+      stock &&
+      cost_by_unit &&
+      description &&
+      category &&
+      expiration_date &&
+      warranty &&
+      brand &&
+      image &&
+      country
+    ) {
+      console.log(req.body);
+      let product = await Product.create({
+        product_name,
+        stock,
+        cost_by_unit,
+        description,
+        expiration_date,
+        warranty,
+        brand,
+        image,
+        country,
+      });
 
-            var categoryy = await Promise.all(category.map(async c => await Category.findAll({where:{name: c}})))
-            categoryy = categoryy.flat();
-            await product.setCategories(categoryy);
-            return res.json(product);
-        }
-    }catch(e){
-        return res.json("ERROR");
+      var categoryy = await Promise.all(
+        category.map(
+          async (c) => await Category.findAll({ where: { name: c } })
+        )
+      );
+      categoryy = categoryy.flat();
+      await product.setCategories(categoryy);
+      return res.json(product);
     }
-        
-    
-})
-
+  } catch (e) {
+    return res.json("ERROR");
+  }
+});
 
 module.exports = router;

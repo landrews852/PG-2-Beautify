a82c6796bd2cb727f5c8b00d2ac7a46a3804ede2
Jair Avila <jair_p1020@hotmail.com>
2022-02-14 22:04:08 -0500

    carrusel 1.0

--- client/src/components/carousel/index.js
+++ client/src/components/carousel/index.js
@@ -27,8 +27,7 @@ export default function Slideshow ()  {
         <div>   
 <Carousel>
     { 
-        arrcarrusel.map ((e)=>{          
-        return ( 
+        arrcarrusel.map ((e)=>( 
            <Carousel.Item>
                 <img
                 className="d-block w-100" width="100%" height="400px"
@@ -39,45 +38,8 @@ export default function Slideshow ()  {
                 <p>{e.text}</p>
             </Carousel.Caption>
             </Carousel.Item>
-        )})
+        ))
     }
-
-    
-{/*     <Carousel.Item>
-        <img
-        className="d-block w-100" width="100%" height="400px"
-        src="https://images2.revistafama.com/tJ9jvfPWHSF_Hs1D2yz6-NBaHp0=/958x596/uploads/media/2019/07/03/que-son-las-pestanas-de-1.jpg"
-        alt="First slide"
-        />
-        <Carousel.Caption>
-        <h3>First slide label</h3>
-        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-        <img
-        className="d-block w-100" width="100%" height="400px"
-        src="https://media.glamour.mx/photos/619051d02d97bd4c522a2691/master/w_1600,c_limit/260880.jpg"
-        alt="Second slide"
-        />
-
-        <Carousel.Caption>
-        <h3>Second slide label</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-    </Carousel.Item>
-    <Carousel.Item>
-        <img
-        className="d-block w-100" width="100%" height="400px"
-        src="https://images.ecestaticos.com/ECoBIlRtrxVxWqNbrCW8zJo7zIo=/18x6:2268x1247/1440x810/filters:fill(white):format(jpg)/f.elconfidencial.com%2Foriginal%2F719%2F5c8%2Fb3f%2F7195c8b3f1e6d69995db09a01eb672bd.jpg"
-        alt="Third slide"
-        />
-
-        <Carousel.Caption>
-        <h3>Third slide label</h3>
-        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-        </Carousel.Caption>
-    </Carousel.Item> */}
 </Carousel>
         </div>
     )

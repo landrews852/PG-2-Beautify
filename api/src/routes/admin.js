26e42e4ee4c3957aeb3a2da062b1e806cb8d3be5
Andres Alvarez <aalvarezqu@gmail.com>
2022-02-16 17:08:16 -0500

    routes admin

---
+++ api/src/routes/admin.js
@@ -0,0 +1,64 @@
+const { Router } = require("express");
+const { Admin } = require("../db");
+
+const router = Router();
+
+//Obtiene la data de un admin por su id y la retorna
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const allAdmin = await Admin.findAll({
+      where: {
+        id
+      }
+    })
+    res.json(allAdmin);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//Envia la data de un admin para agregarla a la BD y retorna toda la data
+router.post("/", async (req, res) => {
+  try {
+    const { id, admin_name, password, profile_picture, email, address, phone } = req.body
+    const newAdmin = await Admin.create({
+      id,
+      admin_name,
+      password,
+      profile_picture,
+      email,
+      address,
+      phone
+    })
+    res.json(newAdmin);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+//Utiliza el id para poder modificar cualquier otro dato del admin con dicho id y retorna los datos completos
+router.put("/:id", async (req, res) => {
+  try {
+    const { id } = req.params
+    const { admin_name, password, profile_picture, email, address, phone } = req.body
+    let data = {}
+    if (admin_name !== undefined) data.admin_name = admin_name
+    if (password !== undefined) data.password = password
+    if (profile_picture !== undefined) data.profile_picture = profile_picture
+    if (email !== undefined) data.email = email
+    if (address !== undefined) data.address = address
+    if (phone !== undefined) data.phone = phone
+    const updateAdmin = await Admin.update(data, {
+      where: {
+        id
+      },
+      returning: true
+    })
+    res.json(updateAdmin[1]);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+module.exports = router;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -4,6 +4,7 @@ const { Router } = require('express');
 const productRouter = require('./product.js');
 const categoriesRouter = require('./categories.js');
 const serviceRouter = require('./service.js')
+const adminRouter = require('./admin.js')
 
 const router = Router();
 
@@ -11,7 +12,7 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/api/product', productRouter);
 router.use('/api/categories', categoriesRouter);
-
 router.use('/api/service', serviceRouter);
+router.use('/api/admin', adminRouter);
 
 module.exports = router;

8c3218f1b9549c02ad383989750fffa7e42ffe4d
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-02-16 03:45:35 -0400

    modifico el datamock y agrego Reviews

--- client/src/datamock/prueba.js
+++
@@ -1,8 +0,0 @@
-categories = require('../../../datamock/Categories.json')
-clients = require('../../../datamock/Clients.json')
-order = require('../../../datamock/Order.json')
-products = require('../../../datamock/Products.json')
-products_order = require('../../../datamock/Products_Order.json')
-services = require('../../../datamock/Services.json')
-
-console.log(categories)

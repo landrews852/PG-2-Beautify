77bb784d4c5cb3e108378abc796d91632507e678
Jair Avila <jair_p1020@hotmail.com>
2022-02-16 17:32:10 -0500

    img cate services

--- client/src/components/cardCateServices/index.js
+++ client/src/components/cardCateServices/index.js
@@ -4,12 +4,10 @@ import Style from './cardCateServices.module.css'
 
 export default function CardCateServices({img,servicio}) {
     return (
-        
-        <div className="contenedor">
             <div className={Style.cards}>               
                 <img src={img} alt="Img not found"/>
                 <h3>{servicio}</h3>
             </div>
-        </div>
+
     )
 }

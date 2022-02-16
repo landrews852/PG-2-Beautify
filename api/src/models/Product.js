fc4fa9fc837bc66b83f5f98d4317b732be83130b
Estefany Diaz <estefanydiaz75@gmail.com>
2022-02-16 16:25:19 -0500

    route/get product

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -1,48 +1,46 @@
-const { DataTypes, UUIDV4 } = require('sequelize');
+const { DataTypes, UUIDV4 } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('product', {
-    product_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "product",
+    {
+      product_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      cost_by_unit: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+
+      warranty: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+        allowNull: false,
+      },
+      discount: {
+        type: DataTypes.INTEGER,
+      },
     },
-    stock:{
-      type:DataTypes.INTEGER,
-      allowNull: false
-    },
-    cost_by_unit:{
-      type:DataTypes.FLOAT,
-      allowNull: false
-    },
-    description:{
-      type:DataTypes.STRING,
-      allowNull: false
-    },
-    // expiration_date:{
-    //   type:DataTypes.DATE,
-    // },
-    warranty:{
-        type:DataTypes.INTEGER,
-        allowNull: false
-    },
-    brand:{
-      type:DataTypes.STRING,
-      allowNull: false
-    },
-    image:{
-      type: DataTypes.ARRAY(DataTypes.STRING),
-      allowNull: false
-    },
-    discount: {
-      type: DataTypes.INTEGER
+    {
+      timestamps: false,
     }
-    // country:{
-    //     type:DataTypes.STRING,
-    //   }
-  },
-  {
-    timestamps: false,
-  });
+  );
 };
--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -6,15 +6,10 @@ const router = Router();
 router.get("/", async (req, res) => {
   try {
     const allProduct = await Product.findAll({
-      include: [
-        {
-          model: Category,
-          attributes: ["name_category"],
-          through: {
-            attributes: [],
-          },
-        },
-      ],
+      include: {
+        model: Category,
+        attributes: ["name_category"],
+      },
     });
     return res.json(allProduct);
   } catch (err) {
@@ -29,12 +24,10 @@ router.post("/", async (req, res) => {
     cost_by_unit,
     description,
     category,
-    //expiration_date,
     warranty,
     brand,
     image,
-    discount
-    //country,
+    discount,
   } = req.body;
 
   try {
@@ -44,12 +37,10 @@ router.post("/", async (req, res) => {
       cost_by_unit &&
       description &&
       category &&
-      //expiration_date &&
       warranty &&
       brand &&
       image &&
       discount
-      //country
     ) {
       var categoryy = await Promise.all(
         category.map(
@@ -57,18 +48,17 @@ router.post("/", async (req, res) => {
         )
       );
       categoryy = categoryy.flat();
-      
+
       let product = await Product.create({
         product_name,
         stock,
         cost_by_unit,
         description,
-        //expiration_date,
+
         warranty,
         brand,
         image,
-        discount
-        //country,
+        discount,
       });
 
       product.setCategory(categoryy[0].id);

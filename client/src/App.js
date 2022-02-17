208acfa6371a6fa78997a1bf59fc9de503c5ebdc
landrews852 <85716642+landrews852@users.noreply.github.com>
2022-02-17 14:58:27 -0300

    arreglo navbar y contacto

--- client/src/App.js
+++ client/src/App.js
@@ -7,28 +7,25 @@ import Market from './components/market';
 import React from 'react';
 import Navigator from './components/navbar';
 import Footer from './components/footer';
-<<<<<<< Updated upstream
 import ProductDetail from './components/productDetail/productDetail';
-=======
 import Contact from './components/contact';
->>>>>>> Stashed changes
+
 
 function App() {
   return (
     <>
     <Router>
       <div className="App">
-        < Navigator /> {/* se agregó la barra a todas las páginas */}
+        <div className="navigator">
+        < Navigator className="navigator" /> {/* se agregó la barra a todas las páginas */}
+        </div>
         <Routes>
           <Route  path="/" element={<Home />} />
           <Route  path="/market" element={<Market />} />          
-<<<<<<< Updated upstream
           <Route  path="/services" element={<Services/>}/>
           <Route  path= "/home/:id" element={<ProductDetail/>} />
-=======
           <Route  path="/services" element={<Services />}/>
           <Route  path="/contact" element={<Contact />}/>
->>>>>>> Stashed changes
         </Routes>
         < Footer /> {/* se agregó el footer a todas las páginas */}
       </div>
--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -8,49 +8,34 @@ import { Link } from 'react-router-dom';
 
 export default function Navigator () {
   return (
-    <Navbar background="#9f8189" variant="light" expand="lg">
+    <Navbar variant="light" expand="lg">
       <Container>
         <Navbar.Brand href="/"><img src={logo} height="100px" alt="logo" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-<<<<<<< Updated upstream
-            <Link className='nav-link' to="/">Home</Link>
-            {/* <NavDropdown title="Products" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#product 1">product 1</NavDropdown.Item>
-              <NavDropdown.Item href="#product 2">product 2</NavDropdown.Item>
-              <NavDropdown.Item href="#product 3">product 3</NavDropdown.Item>
-              {/* <NavDropdown.Divider /> */}
-              {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
-            {/*</NavDropdown> */}
+            {/* <Link className='nav-link' to="/">Home</Link>
             <Link className='nav-link' to="/market"><span>Productos</span></Link>
-            <Link className='nav-link' to="/services"><span>Servicios</span></Link>
-            {/* <NavDropdown title="Services" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#service 1">service 1</NavDropdown.Item>
-              <NavDropdown.Item href="#service 2">service 2</NavDropdown.Item>
-              <NavDropdown.Item href="#service 3">service 3</NavDropdown.Item>
-            </NavDropdown> */}
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#login"> <ButtonLogin /> </Nav.Link>
-=======
+            <Link className='nav-link' to="/services"><span>Servicios</span></Link> */}
             <Nav.Link href="/">Home</Nav.Link>
-            <NavDropdown title="Products" id="basic-nav-dropdown">
+            <Nav.Link href="/market">Productos</Nav.Link>
+            <Nav.Link href="/services">Servicios</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link> <ButtonLogin /> </Nav.Link>
+            {/* <NavDropdown title="Products" id="basic-nav-dropdown">
               <NavDropdown.Item href="/product1">product 1</NavDropdown.Item>
               <NavDropdown.Item href="/product2">product 2</NavDropdown.Item>
-              <NavDropdown.Item href="/product3">product 3</NavDropdown.Item>
+              <NavDropdown.Item href="/product3">product 3</NavDropdown.Item> */}
               {/* <NavDropdown.Divider /> */}
               {/* <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
-            </NavDropdown>
+            {/* </NavDropdown>
             <NavDropdown title="Services" id="basic-nav-dropdown">
               <NavDropdown.Item href="/service 1">service 1</NavDropdown.Item>
               <NavDropdown.Item href="/service 2">service 2</NavDropdown.Item>
               <NavDropdown.Item href="/service 3">service 3</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-            <Nav.Link> <ButtonLogin /> </Nav.Link>
->>>>>>> Stashed changes
+            </NavDropdown> */}
           </Nav>
-            <SearchBar align='right'/>
+            <SearchBar/>
         </Navbar.Collapse>
       </Container>
     </Navbar>

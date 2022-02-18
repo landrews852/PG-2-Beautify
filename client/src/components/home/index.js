af0f67acedea777d506f28f20b37a0fec5d96e14
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-02-18 20:29:39 -0300

    created homeproducts as component

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -4,40 +4,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Card from "../card";
 import Slideshow from "../carousel";
-import { allProducts } from "../../redux/actions";
 import "./home.css"
 import Homeservices from "../homeservices";
+import HomeProducts from "../homeproducts/homeproducts";
 // let allProducts = require('../../datamock/Products.json')
 
 function Home() {
-  const dispatch = useDispatch();
-  let product = useSelector((state) => state.products);
-  useEffect(() => {
-    dispatch(allProducts());
-  }, []);
+  
 
   return (
     <div className="home">
       <Slideshow />
       <Homeservices />
       <h2>Products</h2>
-      {/* COMENTO LO DE ABAJO HASTA QUE SE CREE LA RUTA DE PRODUCTOS ASI NO SE ROMPE LA PAGINA ENTERA */}
+      <HomeProducts />
 
-      {product &&
-        product.slice(0, 5).map((c) => {
-          return (
-            <Link key={c.id} to={"market/" + c.id}>
-              {" "}
-              {/* Cambio de c.code a c.id, ya que se cambio la propiedad en el objeto*/}
-              <Card
-                name={c.product_name}
-                image={c.image}
-                cost_by_unit={c.cost_by_unit}
-                id={c.id}
-              />
-            </Link>
-          );
-        })}
+      
     </div>
   );
 }

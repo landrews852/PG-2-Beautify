e6d970ed551cff20ea0c33045e443a9e670a2a33
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-02-19 00:23:57 -0300

    carousel v3

--- client/src/components/homeproducts/homeproducts.jsx
+++ client/src/components/homeproducts/homeproducts.jsx
@@ -19,8 +19,7 @@ export default function HomeProducts  () {
     // },[dispatch]); 
     
     useMemo(() => {
-        dispatch(allProducts());
-        
+        dispatch(allProducts());        
       },[dispatch]);
        
     

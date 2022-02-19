293973592407f145aef84923897c0cbf0d683d8a
FlorFleitas <flor1197@hotmail.com>
2022-02-18 23:30:44 -0300

    css

--- client/src/components/cardCateServices/index.js
+++ client/src/components/cardCateServices/index.js
@@ -1,17 +1,22 @@
 import React from "react";
-import Style from './cardCateServices.module.css'
+import './cardCateServices.css'
+
+
 
 
 export default function CardCateServices() {
-   return ( <section class="content-wrapper">
+  
+  return ( <div class="content-wrapper">
 <ul id="accordion">
     <li class="acc-item">
+      <h1>FACIAL</h1>
         <div class="acc-item_content">
             <h3>FACIAL</h3>
             <p>Limpiezas faciales, extracciones, peeling, mesoterapia, hilos tensores y muchos más! Descubrí nuestra variedad de tratamientos faciales para revivir tu piel como nunca antes. Contamos con profesionales calificados y productos de primera calidad. Te estamos esperando!</p>
         </div>
     </li>
     <li class="acc-item">
+    <h1>PESTAÑAS</h1>
         <div class="acc-item_content">
             <h3>PESTAÑAS</h3>
             <p>Lifting y tinte de pestañas, extensiones de pestañas y más. Las pestañas que siempre soñaste las encontras en Beautify! Contamos con todos los volumenes y curvaturas del mercado. Cambia tu mirada HOY, elige tus favoritas y contactanos!
@@ -19,6 +24,7 @@ export default function CardCateServices() {
         </div>
     </li>
     <li class="acc-item">
+    <h1>CEJAS</h1>
         <div class="acc-item_content">
             <h3>CEJAS</h3>
             <p>Microblading, laminado, tinte y diseño de cejas. Elige tu método preferido y nuestros profesionales se encargarán de brindarte un servicio personalizado según tu rostro. Observa más detalles en nuestra web!</p>
@@ -26,13 +32,7 @@ export default function CardCateServices() {
     </li>
 </ul>
 
-</section>
- /*    return (
-            <div className={Style.cards}>               
-                <img src={img} alt="Img not found"/>
-                <h3>{servicio}</h3>
-            </div>
-
-    ) */)
+</div>
+) 
 }
 

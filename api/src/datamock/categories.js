65c2ad7b0739e81d7ac50c00106f5668804bb207
Estefany Diaz <estefanydiaz75@gmail.com>
2022-02-18 22:11:15 -0500

    preload product and category db

---
+++ api/src/datamock/categories.js
@@ -0,0 +1,19 @@
+const categories = [
+  { name_category: "Trippledex" },
+  { name_category: "Rank" },
+  { name_category: "Namfix" },
+  { name_category: "Zoolab" },
+  { name_category: "Flexidy" },
+  { name_category: "Solarbreeze" },
+  { name_category: "Greenlam" },
+  { name_category: "Konklab" },
+  { name_category: "Duobam" },
+  { name_category: "Lotlux" },
+  { name_category: "cejas" },
+  { name_category: "pestanas" },
+  { name_category: "cuidado facial" },
+];
+
+module.exports = {
+  categories,
+};

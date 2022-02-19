6d8cf98a9ebb53e7b53c60baa2d55763fdad119f
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-02-18 23:38:48 -0300

    owlcarousel

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,8 +1,4 @@
 import React from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import Card from "../card";
 import Slideshow from "../carousel";
 import "./home.css"
 import Homeservices from "../homeservices";
@@ -16,13 +12,9 @@ function Home() {
     <div className="home">
       <Slideshow />
       <Homeservices />
-<<<<<<< HEAD
-      <h2>Products</h2>
-      <HomeProducts />
-=======
       <h2>PRODUCTOS</h2>
-      {/* COMENTO LO DE ABAJO HASTA QUE SE CREE LA RUTA DE PRODUCTOS ASI NO SE ROMPE LA PAGINA ENTERA */}
->>>>>>> be93ac45aee86931930ea382518bc52376e1de1f
+      <HomeProducts />
+
 
       
     </div>

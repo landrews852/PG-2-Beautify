2fef70ce4e7caeebd7b2ba70558a508ba2deca35
Aura María Bohorquez V <auribohorquez@gmail.com>
2022-02-18 22:12:55 -0500

    creacion funcion loadDB

---
+++ api/src/loadDB/loadDB.js
@@ -0,0 +1,29 @@
+const { Client } = require('../db');
+
+const loadClient = async ()=> {
+  try{
+    const infoClient = require('../datamock/Clients_back.json');
+    
+    await Promise.all(
+    infoClient.map(async cl=> {
+      await Client.create({
+        id: cl.id,
+        name_client: cl.name_client,
+        lastname_client: cl.lastname_client,
+        profile_picture: cl.profile_picture[0],
+        password: cl.password,
+        email: cl.email,
+        address: cl.address,
+        phone: cl.phone,
+        birthday: cl.birthday,
+      })
+    }))
+
+    console.log("Cliente Creado");
+
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+module.exports = loadClient;

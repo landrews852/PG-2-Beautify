b17dade968f0424d8c4bef10621a4a7444322754
Aura María Bohorquez V <auribohorquez@gmail.com>
2022-02-19 19:57:40 -0500

    index del back

--- api/index.js
+++ api/index.js
@@ -22,10 +22,7 @@ const { conn } = require("./src/db.js");
 const { loadCategories } = require("../api/loadData/categoryLoad");
 const { loadProducts } = require("../api/loadData/productLoad");
 // Syncing all the models at once.
-conn.sync({ force: true }).then( async() => {
-  // await loadDB();
-  // console.log("DataBase loaded");
-
+conn.sync({ force: true }).then(() => {
   server.listen(3001, async () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
     loadCategories();

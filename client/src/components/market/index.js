aa837997283e201dcdb26240230f395b0518b7c8
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-02-20 15:54:17 -0400

    listo paginado, falta añadirlo a un componente individual y adaptarlo a la paleta de colores

--- client/src/components/market/index.js
+++ client/src/components/market/index.js
@@ -11,22 +11,39 @@ import { Pagination } from 'react-bootstrap'
 
 export default function Market() {
   const dispatch = useDispatch();
-  let [firstPage,setPage] = useState(0)
-  let [lastPage,setLastPage] = useState(9)
-
   let products = useSelector((state) => state.products);
+
+  let [page,setPage] = useState(1)
+  let firstItem = (page - 1) * 9
+  let lastItem = page * 9
+  let lastPage = Math.floor(products.length/9)
+  let paginatedProducts = products.slice(firstItem,lastItem)
 
   useEffect(() => {
     dispatch(allProducts());
   }, []);
-
-  let actualProducts = products.slice(firstPage,lastPage)
+  
 
   return (
     <div className="main">
       <Filter />
+      <Pagination>
+  <Pagination.First onClick={() => setPage(1)}/>
+  <Pagination.Prev disabled={page <= 1} onClick={() => setPage(page-1)}/>
+  <Pagination.Item onClick={() => setPage(1)} hidden={page === 1 || page === 2}>{1}</Pagination.Item>
+  <Pagination.Ellipsis hidden={page === 1 || lastPage <= 3} />
+
+  <Pagination.Item onClick={() => setPage(page - 1)} hidden={page === 1}>{page - 1}</Pagination.Item>
+  <Pagination.Item active>{page}</Pagination.Item>
+  <Pagination.Item onClick={() => setPage(page + 1)} hidden={page === lastPage || lastPage === (page + 1)}>{page + 1}</Pagination.Item>
+
+  <Pagination.Ellipsis hidden={page === lastPage || lastPage <= 3} />
+  <Pagination.Item onClick={() => setPage(lastPage)} hidden={page === lastPage}>{lastPage}</Pagination.Item>
+  <Pagination.Next disabled={page >= lastPage} onClick={() => setPage(page + 1)}/>
+  <Pagination.Last onClick={() => setPage(lastPage)}/>
+</Pagination>
       <div className="Container Market">
-        {actualProducts?.map((product) => (
+        {paginatedProducts?.map((product) => (
             <Link key={product.id} to={"/market/" + product.id}>
               <Card
                 product_name={product.product_name}

b3aad6235625700308f37f57c3a49a9df9347133
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-02-20 14:43:42 -0400

    Empiezo paginado

--- client/src/components/market/index.js
+++ client/src/components/market/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import React from "react";
 import Card from "../card";
@@ -6,26 +6,32 @@ import { Link } from "react-router-dom";
 import "./market.css";
 import Filter from "../filter";
 import { allProducts } from "../../redux/actions";
+import { Pagination } from 'react-bootstrap'
 // let allProducts = require('../../datamock/Products.json')
 
 export default function Market() {
   const dispatch = useDispatch();
+  let [firstPage,setPage] = useState(0)
+  let [lastPage,setLastPage] = useState(9)
+
   let products = useSelector((state) => state.products);
+
   useEffect(() => {
     dispatch(allProducts());
   }, []);
-  console.log("productos",typeof products)
+
+  let actualProducts = products.slice(firstPage,lastPage)
+
   return (
     <div className="main">
       <Filter />
       <div className="Container Market">
-        {typeof products === "string"?<div>Sin categorias</div>:
-          products.map((p) => (
-            <Link key={p.id} to={"/market/" + p.id}>
-            <Card
-              product_name={p.product_name}
-              image={p.image}
-              cost_by_unit={p.cost_by_unit}
+        {actualProducts?.map((product) => (
+            <Link key={product.id} to={"/market/" + product.id}>
+              <Card
+                product_name={product.product_name}
+                image={product.image}
+                cost_by_unit={product.cost_by_unit}
               />
             </Link>
           ))}

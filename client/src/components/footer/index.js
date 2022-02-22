6a67044f454415b869eb465573f39f8e48e63d5e
FlorFleitas <flor1197@hotmail.com>
2022-02-22 17:42:55 -0300

    css

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import logo from '../../images/logo2.png';
 import s from "./Footer.module.css"
 import { Link } from "react-router-dom";
-import ig from "../../images/ig_logo.png"
-import fb from "../../images/fb_logo.png"
-import mail from "../../images/mail_logo.png"
+import ig from "../../images/instagram.png"
+import fb from "../../images/facebook.png"
+import mail from "../../images/mail.png"
 
 export default function Footer() {
 
@@ -16,9 +16,9 @@ export default function Footer() {
               </Link>
             </div>
             <ul className={s.lista}>
-                <li className={s.li}><a href="https://www.instagram.com/"><img width='50px' src={ig} /></a></li>
-                <li className={s.li}><a href="https://www.facebook.com/"><img width='50px' src={fb} /></a></li>
-                <li className={s.li}><a href="mailto:beautify@correo.com"><img width='50px' src={mail} /></a></li>
+                <li className={s.li}><a href="https://www.instagram.com/"><img width='30px' src={ig} /></a></li>
+                <li className={s.li}><a href="https://www.facebook.com/"><img width='28px' src={fb} /></a></li>
+                <li className={s.li}><a href="mailto:beautify@correo.com"><img width='31px' src={mail} /></a></li>
             </ul>
         </div>
     )

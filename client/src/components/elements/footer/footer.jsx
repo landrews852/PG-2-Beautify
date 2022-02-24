fdcfb04e9b1c44998061b2b69d8d9b1921b47201
FlorFleitas <flor1197@hotmail.com>
2022-02-23 22:12:12 -0300

    correo

--- client/src/components/elements/footer/footer.jsx
+++ client/src/components/elements/footer/footer.jsx
@@ -5,7 +5,8 @@ import { Link } from "react-router-dom";
 import ig from "../../../images/instagram.png"
 import fb from "../../../images/facebook.png"
 import mail from "../../../images/mail.png"
-import {CopyToClipboard} from "react-copy-to-clipboard";
+import {Toaster, toast} from "react-hot-toast";
+import {CopyToClipboard} from "react-copy-to-clipboard"
 
 export default function Footer() {
 
@@ -21,8 +22,11 @@ export default function Footer() {
                 <li className={s.li}><a href="https://www.facebook.com/"><img width='28px' src={fb} /></a></li>
                 <li className={s.li}><div>
     <CopyToClipboard text= "beauty@email.com">
-        <p onClick={(() => alert("correo copiado al portapapeles"))}><img width='30px' src={mail} /></p> 
+        <p onClick={(() => toast.success("Correo copiado al portapapeles"))}><img width='30px' src={mail} /></p> 
     </CopyToClipboard>
+    <Toaster 
+    position="bottom-center"
+    reverseOrder={false} />
 </div></li>
             </ul>
         </div>

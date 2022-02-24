d8a181e486be12d54fab737ec727637140d1d2e4
landrews852 <85716642+landrews852@users.noreply.github.com>
2022-02-24 13:41:36 -0300

    detalles css

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -140,9 +140,6 @@ export default function rootReducer(state = initialState, action) {
       };
 
     case FILTER_BY_OFFER:
-      // const filteredProducts = state.allProducts.filter(product => {
-      //   return product.offert === action.payload.offert
-      // })
       return {
         ...state,
         products: action.payload,

62231517e8efef27f56c21367711ca9f40117e36
FlorFleitas <flor1197@hotmail.com>
2022-02-24 17:59:27 -0300

    css cards home

--- client/src/components/pages/client/home/homeProducts/homeproducts.jsx
+++ client/src/components/pages/client/home/homeProducts/homeproducts.jsx
@@ -5,7 +5,7 @@ import OwlCarousel from "react-owl-carousel";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import { Link } from "react-router-dom";
-import Card from "../../../../cards/productCard/productCard";
+import Card from "../../../../cards/cardHome/cardHome";
 import { useMemo } from "react";
 
 export default function HomeProducts() {

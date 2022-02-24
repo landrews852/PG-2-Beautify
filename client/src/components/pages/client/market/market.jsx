9c5e41519bb338470c6b58936f84cf0581305130
FlorFleitas <flor1197@hotmail.com>
2022-02-24 18:04:50 -0300

    input arreglado, no va a detalle

--- client/src/components/pages/client/market/market.jsx
+++ client/src/components/pages/client/market/market.jsx
@@ -43,14 +43,13 @@ export default function Market() {
           />
           <div className="Container Market">
             {paginatedProducts.map((product) => (
-              <Link key={product.id} to={"/market/" + product.id}>
                 <Card
                   id={product.id}
                   product_name={product.product_name}
                   image={product.image}
                   cost_by_unit={product.cost_by_unit}
                 />
-              </Link>
+              
             ))}
           </div>
         </>

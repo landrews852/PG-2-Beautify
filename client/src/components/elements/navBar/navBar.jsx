7bc85671ba30d6cfa7840179dbb1f134705606b8
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-02-25 18:09:54 -0300

    nofixes

---
+++ client/src/components/elements/navBar/navBar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { Navbar, Container, NavDropdown, Nav } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import logo from "../../../images/logo2.png";
+import SearchBar from "../../features/searchbar/searchBar";
+import ButtonLogin from "../buttons/loginButton/loginButton";
+import Cart_logo from "./cartLogo";
+import s from "./navBar.module.css";
+import { Link } from "react-router-dom";
+import UserMenu from "../userMenu/userMenu";
+import { useLocation } from "react-router-dom";
+
+export default function Navigator() {
+  const location = useLocation();
+  // console.log();
+  return (
+    <Navbar className={s.navBarr} variant="light" expand="lg">
+      <Container>
+        <Link to="/">
+          <Navbar.Brand>
+            <img src={logo} height="100px" alt="logo" />
+          </Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className={s.me_auto}>
+            <Link to="/aboutUs" className="nav-link">
+              Quienes somos
+            </Link>
+            <Link to="/market" className="nav-link">
+              Productos
+            </Link>
+            <Link to="/services" className="nav-link">
+              Servicios
+            </Link>
+          </Nav>
+          {location.pathname !== "/" ? (
+            <SearchBar className={s.navSearch} />
+          ) : null}
+
+          <Cart_logo />
+          <UserMenu />
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}

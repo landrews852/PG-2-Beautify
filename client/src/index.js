b717309277e1b4f1e7ff7496f33a50f5e570984f
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-02-25 23:14:42 -0300

    la que costo mucho

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react'
 // import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './store/index';
@@ -11,12 +12,21 @@ const client_id = "cAkrYWSsneDsy2bOTV2iiLppFYJBeL2Y"
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>    
-    <App /> 
+    <React.StrictMode>
+    <Auth0Provider 
+      domain={domain} 
+      clientId={client_id} 
+      redirectUri={window.location.origin}
+      audience='https://beautify-api'
+      scope='openid profile email'
+    >
+      <App />
+    </Auth0Provider>  
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
+
 
 
 

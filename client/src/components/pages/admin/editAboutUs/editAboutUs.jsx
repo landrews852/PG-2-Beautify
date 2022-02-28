e4c05fbe31fdf6e3786cdbf076cce4a579f02800
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-02-28 11:46:22 -0400

    Modifico componente aboutus con nuevas input y estructurado

--- client/src/components/pages/admin/editAboutUs/editAboutUs.jsx
+++ client/src/components/pages/admin/editAboutUs/editAboutUs.jsx
@@ -28,22 +28,21 @@ const handleSubmit = (e) => {
         <h2 className={s.about}>Edite aqui su mensaje acerca de la empresa:</h2>
             <form className={s.form} onSubmit={(e) => handleSubmit(e)}>
                 <div className={s.aboutContainer}>
-                    <div>
-                        <label name="title" id="title">Encabezado</label>
-                        <input onChange={handleOnChange} type="text" name="title" id="title" />
-                    </div>
-                    <div>
-                        <label name="description" id="description">Aquí podra escribir un mensaje describiendo a que se dedica la empresa, su historia, su visión y misión.</label>
-                    </div>
-                    <textarea onChange={handleOnChange} type="text" name="description" id="description" />
-                    <div>
-                        <label name="slogan" id="slogan">Su eslogan</label>
-                        <textarea onChange={handleOnChange} type="text" name="slogan" id="slogan" />
-                    </div>
-                        <div>
-                        <label name="title" id="title">Url de la imagen anexada</label>
-                    </div>
-                        <input onChange={handleOnChange} type="text" name="image" id="image" />
+
+                    <label name="title" id="title">Encabezado</label>
+                    <input onChange={handleOnChange} type="text" name="title" id="title"/>
+
+                    <label name="description" id="description" className={s.description}>descripción</label>
+                    <textarea onChange={handleOnChange} type="text" name="description" id="description" 
+                    placeholder="Aquí podra escribir un mensaje describiendo a que se dedica la empresa, su historia, su visión y misión." 
+                    />
+                
+                    <label name="slogan" id="slogan" className={s.slogan}>Su eslogan</label>
+                    <textarea onChange={handleOnChange} type="text" name="slogan" id="slogan" />
+                
+                    <label name="image" id="image">Url de la imagen anexada</label>
+                    <input onChange={handleOnChange} type="text" name="image" id="image" />
+                    
                 </div>
                 <button type="submit">Modificar</button>
             </form>

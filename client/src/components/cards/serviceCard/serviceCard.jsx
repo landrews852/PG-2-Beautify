8b9c8a5bc399098d1462caee9fd9dda71712a129
FlorFleitas <flor1197@hotmail.com>
2022-03-03 00:40:05 -0300

    boton agendar cita dinamico

--- client/src/components/cards/serviceCard/serviceCard.jsx
+++ client/src/components/cards/serviceCard/serviceCard.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getServices } from "../../../redux/actions";
 import Loading from "../../elements/loading/loading";
 
+
 export default function CardService() {
   const dispatch = useDispatch();
   useEffect(() => {
@@ -11,10 +12,10 @@ export default function CardService() {
   }, []);
   const services = useSelector((state) => state.services);
   const isLoading = useSelector((state) => state.isLoading);
-
+  
   return (
     <div id={Style.cont}>
-      {isLoading?<Loading/>:services.map((service) => (
+      {isLoading ? <Loading /> : services.map((service) => (
         <div key={service.id} id={Style.container}>
           <div className={Style["product-details"]}>
             <div className={Style["title-stars"]}>
@@ -36,20 +37,20 @@ export default function CardService() {
                 <i
                   className={[Style.fa, Style["fa-star-half-o"]].join(" ")}
                   aria-hidden="true"
-                ></i>
+                  ></i>
                 <i
                   className={[Style.fa, Style["fa-star-o"]].join(" ")}
                   aria-hidden="true"
-                ></i>
+                  ></i>
               </span>
             </div>
             <p className={Style.information}>{service.description}</p>
+            
 
             <div className={Style.control}>
-              <h5 className={Style.information}>
-                Contáctenos al correo para solicitar servicio
-              </h5>
-              {/* <Button quote={'Solicitalo'}/> */}
+           {console.log(service.name_service.replace(/\s+/g, "-"))}
+
+            <a href={ `https://calendly.com/beautify_pg/${service.name_service.replace(/\s+/g, "-")}`} target="_blank">Agenda tu cita</a>
             </div>
           </div>
 
@@ -57,7 +58,8 @@ export default function CardService() {
             <img src={service.image[0]} alt="Omar Dsoky" />
           </div>
         </div>
-      ))}
-    </div>
+  ))
+}
+    </div >
   );
 }

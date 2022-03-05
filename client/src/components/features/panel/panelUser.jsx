e86ab4d50c937b1b6e8aa78d8309d4f8b38f8fd4
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-03-05 19:51:28 -0300

    user side bar

--- client/src/components/features/panel/panelUser.jsx
+++ client/src/components/features/panel/panelUser.jsx
@@ -1,36 +1,11 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useLocation } from "react-router-dom";
-import { cleanCart, getOrders } from "../../../redux/actions";
-import CardOrder from "../../cards/cardOrder/CardOrder.jsx";
+import React from "react";
+import UserSideBar from "../../pages/config/userSideBar/userSideBar";
 
 export default function PanelUser() {
-  const dispatch = useDispatch();
-  const location = useLocation().search;
-  const userlocal = JSON.parse(localStorage.getItem("user"));
-  let { id } = userlocal[0];
-  let orders = useSelector((state) => state.orders);
-  console.log(orders);
-  if (location.toString() === "?true") dispatch(cleanCart());
 
-  useEffect(() => {
-    dispatch(getOrders(id));
-  }, []);
   return (
     <>
-      {orders.length ? (
-        orders.map((o) => (
-          <CardOrder
-            order_date={o.order_date}
-            key={o.id}
-            total_amount={o.total_amount}
-            products={o.products}
-            address={o.address}
-          />
-        ))
-      ) : (
-        <p>No tienes ordenes.</p>
-      )}
+      < UserSideBar/>
     </>
   );
 }

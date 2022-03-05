ed8f467e366efff7bab0f22fc7371bc16229fffa
Andres Alvarez <aalvarezqu@gmail.com>
2022-03-04 20:41:21 -0500

    order

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -29,7 +29,7 @@ router.get('/', async (req,res) => {
             ["order_date", "DESC"]
         ]
     })
-    res.json(data)
+        res.json(data)
     } catch (error) {
         console.log(error)
     }

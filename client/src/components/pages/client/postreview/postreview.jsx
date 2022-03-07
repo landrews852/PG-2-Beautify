5285b9eb155f7664c8a0858191a75d39b853f41f
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-03-07 07:42:33 -0300

    postreview first validate

--- client/src/components/pages/client/postreview/postreview.jsx
+++ client/src/components/pages/client/postreview/postreview.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { postReview } from '../../../../redux/actions';
@@ -6,8 +6,25 @@ import s from './postreview.module.css'
 import { useAuth0 } from '@auth0/auth0-react';
 import Swal from 'sweetalert2';
 
+
+const validate = (review) => {
+  let errors = {};
+  
+  if (!review.comment) {
+    errors.comment = "Debe completar con un mensaje";
+  } else if (!/^[A-Z][\s\w\W]{1,250}$/.test(review.comment)) {
+    errors.comment = "El apellido debe empezar en mayuscula";
+  }
+  if (!review.rank) {
+    errors.rank = "Elija un numero para puntuar";
+  }
+  return errors;
+};
+
+
 export default function PostReview(props) {
 const dispatch = useDispatch();
+const [errors, setErrors] = useState({});
 const reviewpost = {
   rank: "",
   comment: "",
@@ -15,6 +32,13 @@ const reviewpost = {
 const {user , id}  = props; 
 const [review,setReview] = useState(reviewpost);
 const { getAccessTokenSilently } = useAuth0();
+
+
+useEffect(() => {
+  setErrors(validate(review));
+}, [review]);
+
+
 
 const handleChange = (e) => {
   if(e.target.name === "rank"){
@@ -50,10 +74,16 @@ const handleChange = (e) => {
                   <option value="3">3</option>
                   <option value="4">4</option>
                   <option value="5">5</option>
-              </select>    
+              </select>
+              {errors.rank && (
+              <p className={s.error}>{errors.rank}</p>
+            )}    
             </div>
               <textarea value={review.comment} name="comment" onChange={(e) => {handleChange(e)}}>
-              </textarea>            
+              </textarea>
+              {errors.comment && (
+              <p className={s.error}>{errors.comment}</p>
+            )}            
         </div>
         <button type="submit" className={s.button}>Publicar</button>
       </form>     

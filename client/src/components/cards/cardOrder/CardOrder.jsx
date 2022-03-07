20f72ba8b85c262cab85e02474bd8f681c7ed69b
RosLauga <88462137+RosLauga@users.noreply.github.com>
2022-03-06 23:38:59 -0300

    fixed css orders and ordersdetail

--- client/src/components/cards/cardOrder/CardOrder.jsx
+++ client/src/components/cards/cardOrder/CardOrder.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ModalComp from "../../elements/modal/modal";
 import s from "./cardOrder.module.css";
 
 function CardOrder({ total_amount, address, order_date, id, status, getDetail }) {
@@ -8,22 +7,39 @@ function CardOrder({ total_amount, address, order_date, id, status, getDetail })
   return (
     <>
       <div className={s.container} onClick={() => getDetail(id)}>
-        <div className={s.orderId}>
-          {id}
-        </div>
-        <div className={s.date}>
-          {order_date.slice(0,10)}
-        </div>
-        <div className={s.time}>
-          {order_date.slice(11,19)}  
-        </div>
-        <div className={s.total}>
-          {total_amount}
-        </div>
-        <div className={s.status}>
-          {status}
-        </div>
-      </div>
+          <div className={s.containerorder}>
+          <div className={s.containerdata}>
+            <label>id de la orden</label>
+            <div className={s.orderId}>
+              {id}
+            </div>
+          </div>
+          <div className={s.containerdata}>
+              <label>Fecha</label>
+              <div className={s.date}>
+                {order_date.slice(0,10)}
+              </div>
+            </div> 
+            <div className={s.containerdata}>
+                <label>Hora</label>
+                <div className={s.time}>
+                  {order_date.slice(11,19)}  
+                </div>
+            </div>
+            <div className={s.containerdata}>
+              <label>Total</label>
+              <div className={s.total}>
+                {total_amount}
+              </div>
+            </div>
+            <div className={s.containerdata}>  
+              <label>Estado</label>
+              <div className={s.status}>
+                {status}
+              </div>
+            </div>  
+          </div> 
+        </div>     
     </>
   );
 }

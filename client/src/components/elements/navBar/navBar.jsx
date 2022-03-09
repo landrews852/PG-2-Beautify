6f37042eeb4b558dd9689533bf8f1f2d201c1e4c
gabriel20x <gabrielgarciatorresm3@gmail.com>
2022-03-09 03:55:24 -0400

    arreglo para mas seguridad

--- client/src/components/elements/navBar/navBar.jsx
+++ client/src/components/elements/navBar/navBar.jsx
@@ -32,6 +32,9 @@ export default function Navigator() {
             if (result.isConfirmed) {
               logout();
               localStorage.clear();
+            } else {
+              logout();
+              localStorage.clear();
             }
           })
         }

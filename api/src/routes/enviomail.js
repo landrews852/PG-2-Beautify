9819a556039195359e5e4df99c46e1b946876817
Jair Avila <jair_p1020@hotmail.com>
2022-03-08 21:11:16 -0500

    send emails

---
+++ api/src/routes/enviomail.js
@@ -0,0 +1,56 @@
+const { Router } = require("express");
+const nodemailer = require ('nodemailer')
+const { google }= require ('googleapis')
+const router = Router();
+const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI ,REFRESH_TOKEN} = process.env;
+
+router.post ('/', (req,res)=>{ 
+  const {name,apellido,email,status,transaction_amount,id_order,items,address}= req.body
+  const oAuth2Client=new google.auth.OAuth2 (CLIENT_ID,CLIENT_SECRET,REDIRECT_URI)
+  
+  oAuth2Client.setCredentials ({refresh_token:REFRESH_TOKEN})
+
+  const sendMail = async ()=>{
+    try{
+      const accessToken= await oAuth2Client.getAccessToken ()
+      const transporter= nodemailer.createTransport ({
+        service:"gmail",
+        auth:{
+          type:'OAuth2',
+          user: 'beautify.pg@gmail.com' ,
+          clientId: CLIENT_ID,
+          clientSecret:CLIENT_SECRET,
+          refreshToken:REFRESH_TOKEN,
+          accessToken:accessToken
+
+        },   
+      })
+        
+      const mailOptions={
+        from: "Beautify <beautify.pg@gmail.com>",
+        to: email,
+        subject:`Estado de su compra`,
+        text:`
+Estimad@ ${name} ${apellido} 👋,
+Estamos muy contentos que hayas encontrado lo que estabas buscando en nuestra tienda 😊, con el id_orden puedes revisar mas detalles de tu compra en nuestra pagina web.
+
+id_orden: ${id_order},
+Estado: ${status},
+Dirección de envio: ${address},
+Valor total: ${transaction_amount}
+
+!Gracias por comprar en nuestra tienda! 🌸`
+      }
+      const result= await  transporter.sendMail (mailOptions)
+      return result 
+    }catch (err){
+      console.log (err)
+    }
+  }
+  sendMail ()
+  .then (result=> res.status(200).send ('enviado'))
+  .catch (error=> console.log (error.message));
+
+})
+
+module.exports = router;
